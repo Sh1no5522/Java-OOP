55955bfd54904134aee326e9a1eaac110a6913f4
Sh1no5522 <your-email@example.com>
2026-03-16 00:44:59 +0600

    Cleaned repository: Added only Lab 1 and Lab 2

--- programming/python/Python_Programs/sequence_max.py
+++
@@ -1,8 +0,0 @@
-def maxSequence(arr):
-    max,curr=0,0
-    for x in arr:
-        curr+=x
-        if curr<0:curr=0
-        if curr>max:max=curr
-    return max
-print(maxSequence([1,2,3,4,5,6,7,8]))
